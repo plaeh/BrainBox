a60ea82317082ee673330df4df479c59f038f05e
Alexia McKenzie <pandapprentice@gmail.com>
2015-04-10 10:36:21 -0700

    fixed - working order

--- BrainBox.ino
+++ BrainBox.ino
@@ -10,24 +10,26 @@
 #define DELTA_HIGH 3
 #define DELTA_MED 2
 #define DELTA_LOW 1
+#define PINK_NOISE 20
 
 int melody[] = {
   ALPHA_LOW, ALPHA_LOW, ALPHA_LOW, ALPHA_LOW, ALPHA_LOW, THETA_HIGH, THETA_HIGH, THETA_HIGH, THETA_HIGH, THETA_HIGH, THETA_LOW, THETA_LOW, THETA_LOW, THETA_LOW, THETA_LOW, DELTA_HIGH, DELTA_MED, DELTA_LOW};
 
 int noteDurations[] = {
-  2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+  2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 2, 4, 4, 8, 4, 8};
 
 void setup() {
-  digitalWrite(0, LOW);
+  analogWrite(0, HIGH);
+  analogWrite(1, HIGH);
 }
 
 void loop() {
   for (int thisNote = 0; thisNote < 18; thisNote++) {
-    int noteDuration = 100000/noteDurations[thisNote];
+    int noteDuration = 5000/noteDurations[thisNote];
     tone(0, melody[thisNote], noteDuration);
 
-    int pauseBetweenNotes = noteDuration * 0.1;
-    delay(pauseBetweenNotes);
-    noTone(0);
+    int pauseBetweenNotes = noteDuration * 1;
+    delay(10);
+    tone(1, PINK_NOISE, pauseBetweenNotes);
   }
 }
